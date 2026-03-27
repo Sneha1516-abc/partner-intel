74512dff26d9d24345063d4b9b18fdf4d59e1505
Sneha Ratagal <sneharatagal7@gmail.com>
2026-03-27 15:06:15 +0530

    Remove test-key.js

--- test-key.js
+++
@@ -1,5 +0,0 @@
-fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyCV43ra84fDjouQFZO8LcEAAykVZ0v1fYI", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ contents: [{ parts: [{ text: "hello" }] }] })
-}).then(r => r.json()).then(data => console.log(JSON.stringify(data, null, 2))).catch(console.error);
